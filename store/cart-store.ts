e1306cf7ae175a533f43529d990d559eaa2f9972
Sepide Kia <sepidekia6990@gmail.com>
2025-03-16 09:52:18 +0330

    ft:add zustand store

---
+++ store/cart-store.ts
@@ -0,0 +1,35 @@
+"use client";
+
+import { Item } from "@/types";
+import { create } from "zustand";
+
+type CartStore = {
+  items: Item[];
+  addItem: (item: Item) => void;
+  clearCart: () => void;
+  totalPrice: () => number;
+};
+
+export const useCartStore = create<CartStore>((set, get) => ({
+  items: [],
+  addItem: (item) => {
+    const existingItem = get().items.find((i) => i.id === item.id);
+
+    if (existingItem) {
+      set((state) => ({
+        items: state.items.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+        ),
+      }));
+    } else {
+      set((state) => ({ items: [...state.items, item] }));
+    }
+  },
+  clearCart: () => set({ items: [] }),
+  totalPrice: () => {
+    return get().items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  },
+}));
