d45b0f057f04b5e237cc12073ce1121165a27375
Sepide Kia <sepidekia6990@gmail.com>
2025-03-16 09:51:21 +0330

    ft:create pages in pages router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import Header from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div dir="rtl">
+      <Header />
+
+      <div className="flex h-screen overflow-hidden border-collapse">
+        <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 flex flex-col bg-muted text-muted-foreground">
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <Toaster />
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,135 @@
-import Image from "next/image";
+import MedicineItem from "@/components/medicine/medicine-item";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Medicine } from "@/types";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { GetServerSideProps } from "next";
 import localFont from "next/font/local";
+import { useRouter } from "next/router";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./fonts/Samim.woff2",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
-export default function Home() {
+interface HomeProps {
+  medicines: Medicine[];
+  totalCount: number;
+  currentPage: number;
+  perPage: number;
+}
+
+export default function Home({
+  medicines,
+  totalCount,
+  currentPage,
+  perPage,
+}: HomeProps) {
+  const totalPages = Math.ceil(totalCount / perPage);
+  const router = useRouter();
+
+  const handlePageChange = (page: number) => {
+    const queryParams = new URLSearchParams(
+      router.query as Record<string, string>
+    );
+
+    if (page === 1) {
+      queryParams.delete("page");
+    } else {
+      queryParams.set("page", String(page));
+    }
+
+    router.push(
+      `/${queryParams.toString() ? "?" + queryParams.toString() : ""}`,
+      undefined,
+      {
+        shallow: false,
+      }
+    );
+  };
+
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} font-[family-name:var(--font-geist-sans)] flex-1 flex flex-col gap-8`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className="w-full max-w-[600px] flex flex-col gap-4 mx-auto ">
+        {medicines.map((medicine) => (
+          <MedicineItem key={medicine.id} medicine={medicine} />
+        ))}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="flex items-center justify-center gap-2">
+          <Button
+            variant="outline"
+            size="icon"
+            disabled={currentPage <= 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+            className="bg-muted border border-transparent shadow-transparent"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <ChevronRight />
+          </Button>
+
+          {Array.from({ length: totalPages }, (_, index) => (
+            <Button
+              key={index + 1}
+              variant="outline"
+              size="icon"
+              onClick={() => handlePageChange(index + 1)}
+              className={cn(
+                "bg-muted border border-transparent shadow-transparent transition-colors",
+                {
+                  "border-secondary text-secondary": currentPage === index + 1,
+                }
+              )}
+            >
+              {index + 1}
+            </Button>
+          ))}
+
+          <Button
+            variant="outline"
+            size="icon"
+            className="bg-muted border border-transparent shadow-transparent"
+            disabled={currentPage >= totalPages}
+            onClick={() => handlePageChange(currentPage + 1)}
           >
-            Read our docs
-          </a>
+            <ChevronLeft />
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  try {
+    const page = parseInt((query.page as string) || "1", 10) || 1;
+    const perPage = 4;
+
+    const res = await fetch(
+      `http://localhost:3001/medicines?_page=${page}&_per_page=${perPage}`
+    );
+    const paginatedData = await res.json();
+    const totalCount = paginatedData.items;
+    const medicines = paginatedData.data as Medicine[];
+
+    return {
+      props: {
+        medicines,
+        currentPage: page,
+        perPage,
+        totalCount,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        medicines: [],
+        totalCount: 0,
+        currentPage: 1,
+        perPage: 4,
+      },
+    };
+  }
+};
